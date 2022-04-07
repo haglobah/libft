cad487723bcf0b48e687227afd839b22c4e0ae21
unknown <hagenlob@posteo.de>
2022-04-07 16:06:00 +0200

    lstadd

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -12,7 +12,15 @@
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-
+	if (!lst || !new)
+		return ;
+	if (*lst)
+	{
+		new->next = *lst;
+		*lst = new;
+	}
+	else
+		*lst = new;
 }
