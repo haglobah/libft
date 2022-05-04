c85f271c7774de26bebd9091de7fbaaabf8387f9
Beat Hagenlocher <bhagenlo@3-F-14.42heilbronn.de>
2022-05-04 17:58:37 +0200

    Math added

---
+++ ft_m1.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_m1.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhagenlo <bhagenlo@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 13:09:42 by bhagenlo          #+#    #+#             */
+/*   Updated: 2022/05/04 13:12:47 by bhagenlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+
+int		ft_min(int a, int b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+int		ft_max(int a, int b)
+{
+	if (a < b)
+		return (b);
+	else
+		return (a);
+}
